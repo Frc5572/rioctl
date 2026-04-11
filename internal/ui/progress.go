c8486ef179e4e11c35a0f4e6918c2a74b8ec5e2f
Alex Resnick <adr8292@gmail.com>
2026-04-11 11:37:51 -0500

    Works great!

--- internal/ui/progress.go
+++ internal/ui/progress.go
@@ -1,6 +1,10 @@
 package ui
 
 import (
+	"fmt"
+	"rioctl/internal/utils"
+	"strings"
+
 	"charm.land/bubbles/v2/progress"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
@@ -18,10 +22,14 @@ var (
 )
 
 type progressModel struct {
-	progress progress.Model
-	total    int
-	current  int
-	done     bool
+	progress         progress.Model
+	done             bool
+	total            int
+	finished         int
+	currentFile      string
+	currentFileBytes int64
+	totalFileBytes   int64
+	completedFiles   []string
 }
 
 func NewProgress(total int) progressModel {
@@ -39,38 +47,71 @@ func (m progressModel) Init() tea.Cmd {
 
 func (m progressModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		return m, tea.Quit
+
+	case tea.WindowSizeMsg:
+		m.progress.SetWidth(msg.Width - padding*2 - 4)
+		if m.progress.Width() > maxWidth {
+			m.progress.SetWidth(maxWidth)
+		}
+		return m, nil
 	case int:
-		m.current = msg
-		if m.current >= m.total {
+		m.finished = msg
+		if m.finished >= m.total {
 			m.done = true
 			return m, tea.Quit
 		}
+	case utils.File:
+		m.currentFileBytes = msg.Current
+		m.totalFileBytes = msg.Size
+		m.currentFile = msg.Name
+		if m.currentFileBytes >= m.totalFileBytes {
+			m.completedFiles = append(m.completedFiles, m.currentFile)
+		}
 	}
 
-	percent := float64(m.current) / float64(m.total)
+	percent := float64(m.finished) / float64(m.total)
 	cmd := m.progress.SetPercent(percent)
 
 	return m, cmd
 }
 
 func (m progressModel) View() tea.View {
-	// pad := strings.Repeat(" ", padding)
-	val := float64(m.current) / float64(m.total)
-	return tea.NewView(m.progress.ViewAs(val))
+	pad := strings.Repeat(" ", padding)
+	// val := float64(m.finished) / float64(m.total)
+	current := float64(m.currentFileBytes) / float64(m.totalFileBytes)
+	a := ""
+	for i, file := range m.completedFiles {
+		a += fmt.Sprintf("%s (%d/%d)\n", file, i+1, m.total)
+	}
+	if m.done {
+		return tea.NewView("\n\n" + a)
+	}
+	return tea.NewView("\n\n" + a +
+		fmt.Sprintf("Downloading %s (%d/%d)", m.currentFile, m.finished+1, m.total) + "\n\n" +
+		m.progress.ViewAs(current) + "\n\n" +
+		pad + helpStyle(fmt.Sprintf("%s / %s", humanize(m.currentFileBytes), humanize((m.totalFileBytes)))))
 }
 
-func RunProgress(total int, updates <-chan int) {
+func RunProgress(total int, current <-chan utils.File, finished <-chan int) {
 	m := NewProgress(total)
 	p := tea.NewProgram(m)
 
 	go func() {
-		for u := range updates {
+		for u := range current {
+			p.Send(u)
+		}
+	}()
+	go func() {
+		for u := range finished {
 			p.Send(u)
 		}
 	}()
 
 	p.Run()
 }
+
 func RunProgress1(total int, stuff []string) {
 	m := NewProgress(total)
 	p := tea.NewProgram(m)
@@ -82,4 +123,8 @@ func RunProgress1(total int, stuff []string) {
 	}()
 
 	p.Run()
+}
+
+func humanize(b int64) string {
+	return fmt.Sprintf("%.2f MB", float64(b)/1024/1024)
 }
